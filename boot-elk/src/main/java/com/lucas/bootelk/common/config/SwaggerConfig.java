d0606ace3d78a2b8b28ee383438f2e03e660d752
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-08-18 19:12:52 +0900

    FIX: swagger 설정 변경

--- boot-elk/src/main/java/com/lucas/bootelk/common/config/SwaggerConfig.java
+++ boot-elk/src/main/java/com/lucas/bootelk/common/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig {
     public GroupedOpenApi api() {
         return GroupedOpenApi.builder()
                 .group("API")
-                .pathsToMatch("/api/**")
+                .pathsToMatch("/**")
                 .build();
     }
 }
