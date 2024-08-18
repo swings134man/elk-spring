59851aaf0d19c275924be3032b7565f6da4694e0
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2024-08-18 16:59:44 +0900

    boot-elk init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring Boot For ELK Stack(Elasticsearch, Logstash, Kibana)
---
+++ boot-elk/src/test/java/com/lucas/bootelk/BootElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.bootelk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootElkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
